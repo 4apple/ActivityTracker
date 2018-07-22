045e1e84ee0ade82160696c14571eb79546a5c27
4apple <4apple@163.com>
2018-07-22 15:33:16 +0800

    update api for sdk 26 or above
    update api for sdk 26 or above

--- app/src/main/java/com/fashare/activitytracker/TrackerWindowManager.java
+++ app/src/main/java/com/fashare/activitytracker/TrackerWindowManager.java
@@ -2,6 +2,7 @@ package com.fashare.activitytracker;
 
 import android.content.Context;
 import android.graphics.PixelFormat;
+import android.os.Build;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
@@ -28,7 +29,13 @@ public class TrackerWindowManager {
         params.width = WindowManager.LayoutParams.WRAP_CONTENT;
         params.height = WindowManager.LayoutParams.WRAP_CONTENT;
         params.gravity = Gravity.LEFT | Gravity.TOP;
-        params.type = WindowManager.LayoutParams.TYPE_PHONE;
+        //begin mango modify on 2018/7-22
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            params.type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        //end mango modify on 2018/7-22
         params.format = PixelFormat.RGBA_8888;
         params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
